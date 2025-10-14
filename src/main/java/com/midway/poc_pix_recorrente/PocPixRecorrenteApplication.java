214c38f68f35dc4f333828d13beaca5887520129
samuelRibeiro91 <156724856+samuelRibeiro91@users.noreply.github.com>
2025-10-14 19:43:38 -0300

    feat:  Start the project in Java Spring Boot

---
+++ src/main/java/com/midway/poc_pix_recorrente/PocPixRecorrenteApplication.java
@@ -0,0 +1,13 @@
+package com.midway.poc_pix_recorrente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocPixRecorrenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocPixRecorrenteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/midway/poc_pix_recorrente/PocPixRecorrenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.midway.poc_pix_recorrente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocPixRecorrenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
